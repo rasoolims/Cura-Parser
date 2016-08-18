4b0b84a1420cd5a224fac6b3aa2a4ab4a1e7ff6e
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-08-18 19:51:49 -0400

    empty input layer

---
+++ src/edu/columbia/cs/nlp/YaraParser/Learning/NeuralNetwork/InputLayer.java
@@ -0,0 +1,16 @@
+package edu.columbia.cs.nlp.YaraParser.Learning.NeuralNetwork;
+
+/**
+ * Created by Mohammad Sadegh Rasooli.
+ * ML-NLP Lab, Department of Computer Science, Columbia University
+ * Date Created: 8/18/16
+ * Time: 7:50 PM
+ * To report any bugs or problems contact rasooli@cs.columbia.edu
+ */
+
+/**
+ * This class shows the input layer which is a concatenation of different embedding layer.
+ */
+public class InputLayer {
+    // todo
+}
